cf4d7f1ad04be2878d0e6a41c65997f6cb4cc47e
OlhoBeata <luis.santos@olhodabeata.com>
2026-08-05 14:32:02 +0100

    Update image URL for better quality preview

--- script.js
+++ script.js
@@ -76,7 +76,8 @@ const encodedPhotoId = photoId
  * O original continua a ser utilizado apenas no download.
  */
 const imageUrl =
-  `https://res.cloudinary.com/${CONFIG.cloudinary.cloudName}/image/upload/c_limit,w_180,q_35,f_auto/${encodedPhotoId}`;
+  `https://res.cloudinary.com/${CONFIG.cloudinary.cloudName}` +
+  `/image/upload/c_limit,w_180,q_30,e_blur:180,f_auto/${encodedPhotoId}`;
 
 const downloadUrl =
     `https://res.cloudinary.com/${CONFIG.cloudName}/image/upload/fl_attachment/${encodedPhotoId}`;
